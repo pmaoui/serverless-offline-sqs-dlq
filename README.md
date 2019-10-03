4ab35339f53896d875a6faca6d01731b589079e2
Pierre Maoui <pierre.maoui@wing.eu>
2019-10-03 18:56:53 +0200

    Create DLQ on serverless offline startup script

---
+++ README.md
@@ -0,0 +1,50 @@
+# serverless-offline-sqs
+
+This Serverless-offline plugin brings [Dead-Letter Queues](https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-dead-letter-queues.html) to AWS λ and SQS queue on your local machine. It extends [serverless-offline-sqs](https://github.com/CoorpAcademy/serverless-plugins/tree/master/packages/serverless-offline-sqs) and thus depends on it.
+
+Warning: Serverless [doesn't support yet SQS for DLQ](https://serverless.com/framework/docs/providers/aws/guide/functions#dlq-with-sqs). After the deployment on AWS, the DLQ needs to be set on AWS (either manually or by a script).
+
+## Installation
+
+First, ensure to have [serverless-offline-sqs](https://github.com/CoorpAcademy/serverless-plugins/tree/master/packages/serverless-offline-sqs) included in your project:
+
+```sh
+npm install serverless-offline-sqs serverless-offline-sqs-dlq
+
+```
+
+Then inside your project's `serverless.yml` file, add following entries to the plugins section before `serverless-offline` (and after `serverless-webpack` if presents).
+
+```yml
+plugins:
+  - serverless-webpack
+  - serverless-offline-sqs
+  - serverless-offline-sqs-dlq
+  - serverless-offline
+```
+
+
+## Configure
+
+Usage:
+```yml
+functions:
+  basicLambda:
+    handler: index.basicLambda
+    events:
+      - sqs: arn:aws:sqs:eu-west-1:XXXXXXX:basicLambdaTriggerQueue
+  handleErrorLambda:
+    handler: index.handleErrorLambda
+    events:
+      - sqs: arn:aws:sqs:eu-west-1:XXXXXXX:handleErrorLambdaQueue
+```
+
+Specify in the `custom` section which function handles errors:
+```yml
+custom:
+  (...)
+  serverless-offline-sqs-dlq:
+    basicLambda:
+      onError: handleErrorLambda
+      maxReceiveCount: 3
+```
---
+++ src/index.js
@@ -0,0 +1,112 @@
+const SQS = require('aws-sdk/clients/sqs');
+
+const extractQueueNameFromARN = arn => {
+  const [, , , , , QueueName] = arn.split(':');
+  return QueueName;
+};
+
+const GET_QUEUES_MAX_RETRIES = 5;
+
+const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
+
+class ServerlessOfflineSQSDLQ {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.service = serverless.service;
+    this.options = options;
+
+    this.hooks = {
+      'before:offline:start:init': this.offlineStartInit.bind(this),
+      'before:offline:start:end': this.offlineStartEnd.bind(this),
+    };
+  }
+
+  offlineStartInit() {
+    this.serverless.cli.log(`Starting Offline SQS DLQ.`);
+
+    const {functions, custom} = this.service;
+    const configDLQ = custom['serverless-offline-sqs-dlq'];
+
+    if (configDLQ) {
+      const client = new SQS({
+        ...custom['serverless-offline-sqs'],
+      });
+
+      Object.keys(configDLQ).forEach(async functionName => {
+        const func = functions[functionName];
+        if (!func) {
+          this.serverless.cli.log(`λ not found: ${functionName}.`);
+          return;
+        }
+        const QueueArn = func.events.map(e => e.sqs).filter(f => f)[0];
+        if (!QueueArn) {
+          this.serverless.cli.log(
+            `λ without SQS event source: ${functionName}.`,
+          );
+          return;
+        }
+        const funcDLQ = functions[configDLQ[functionName].onError];
+        if (!funcDLQ) {
+          this.serverless.cli.log(
+            `λ DLQ not found: ${configDLQ[functionName]}.`,
+          );
+          return;
+        }
+        const DeadLetterQueueArn = funcDLQ.events
+          .map(e => e.sqs)
+          .filter(f => f)[0];
+
+        if (!DeadLetterQueueArn) {
+          this.serverless.cli.log(
+            `λ without SQS event source: ${configDLQ[functionName].onError}.`,
+          );
+          return;
+        }
+
+        this.serverless.cli.log(
+          `Creating DLQ of SQS event source, λ: ${functionName}.`,
+        );
+
+        let attempts = 0;
+        let QueueUrl;
+        while (!QueueUrl && attempts < GET_QUEUES_MAX_RETRIES) {
+          attempts += 1;
+          try {
+            ({QueueUrl} = await client
+              .getQueueUrl({
+                QueueName: extractQueueNameFromARN(QueueArn) + 'lol',
+              })
+              .promise());
+          } catch (e) {
+            this.serverless.cli.log('catch ' + attempts);
+            if (attempts === GET_QUEUES_MAX_RETRIES) {
+              throw e;
+            }
+          }
+          await sleep(1000);
+        }
+        try {
+          await client
+            .setQueueAttributes({
+              QueueUrl,
+              Attributes: {
+                RedrivePolicy: JSON.stringify({
+                  deadLetterTargetArn: DeadLetterQueueArn,
+                  maxReceiveCount: configDLQ[functionName].maxReceiveCount || 3,
+                }),
+              },
+            })
+            .promise();
+        } catch (e) {
+          this.serverless.cli.log(e.message);
+        }
+      });
+    }
+  }
+
+  offlineStartEnd() {
+    this.serverless.cli.log('offline-start-end');
+  }
+}
+
+module.exports = ServerlessOfflineSQSDLQ;

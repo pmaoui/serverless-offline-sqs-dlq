3cf5ced31caf100ed00c46f45da329e458f9d998
Pierre Maoui <maouipierre@gmail.com>
2020-07-21 10:15:39 +0200

    configure prettier

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-const SQS = require('aws-sdk/clients/sqs');
+const SQS = require('aws-sdk/clients/sqs')
 
-const extractQueueNameFromARN = arn => {
-  const [, , , , , QueueName] = arn.split(':');
-  return QueueName;
-};
+const extractQueueNameFromARN = (arn) => {
+  const [, , , , , QueueName] = arn.split(':')
+  return QueueName
+}
 
-const GET_QUEUES_MAX_RETRIES = 5;
+const GET_QUEUES_MAX_RETRIES = 5
 
-const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms))
 
 class ServerlessOfflineSQSDLQ {
   constructor(serverless, options) {
-    this.serverless = serverless;
-    this.service = serverless.service;
-    this.options = options;
+    this.serverless = serverless
+    this.service = serverless.service
+    this.options = options
 
     this.hooks = {
       'before:offline:start:init': this.offlineStartInit.bind(this),
       'before:offline:start:end': this.offlineStartEnd.bind(this),
-    };
+    }
   }
 
   offlineStartInit() {
-    this.serverless.cli.log(`Starting Offline SQS DLQ.`);
+    this.serverless.cli.log(`Starting Offline SQS DLQ.`)
 
-    const {functions, custom} = this.service;
-    const configDLQ = custom['serverless-offline-sqs-dlq'];
+    const { functions, custom } = this.service
+    const configDLQ = custom['serverless-offline-sqs-dlq']
 
     if (configDLQ) {
       const client = new SQS({
         ...custom['serverless-offline-sqs'],
-      });
+      })
 
-      Object.keys(configDLQ).forEach(async functionName => {
-        const func = functions[functionName];
+      Object.keys(configDLQ).forEach(async (functionName) => {
+        const func = functions[functionName]
         if (!func) {
-          this.serverless.cli.log(`λ not found: ${functionName}.`);
-          return;
+          this.serverless.cli.log(`λ not found: ${functionName}.`)
+          return
         }
-        let QueueArn = func.events.map(e => e.sqs).filter(f => f)[0];
+        let QueueArn = func.events.map((e) => e.sqs).filter((f) => f)[0]
         if (typeof QueueArn !== 'string' && QueueArn.arn !== undefined) {
           QueueArn = QueueArn.arn
         }
@@ -46,51 +46,54 @@ class ServerlessOfflineSQSDLQ {
         if (!QueueArn) {
           this.serverless.cli.log(
             `λ without SQS event source: ${functionName}.`,
-          );
-          return;
+          )
+          return
         }
-        const funcDLQ = functions[configDLQ[functionName].onError];
+        const funcDLQ = functions[configDLQ[functionName].onError]
         if (!funcDLQ) {
           this.serverless.cli.log(
             `λ DLQ not found: ${configDLQ[functionName]}.`,
-          );
-          return;
+          )
+          return
         }
         let DeadLetterQueueArn = funcDLQ.events
-          .map(e => e.sqs)
-          .filter(f => f)[0];
+          .map((e) => e.sqs)
+          .filter((f) => f)[0]
 
-          if (typeof DeadLetterQueueArn !== 'string' && DeadLetterQueueArn.arn !== undefined) {
-            DeadLetterQueueArn = DeadLetterQueueArn.arn
-          }
+        if (
+          typeof DeadLetterQueueArn !== 'string' &&
+          DeadLetterQueueArn.arn !== undefined
+        ) {
+          DeadLetterQueueArn = DeadLetterQueueArn.arn
+        }
 
         if (!DeadLetterQueueArn) {
           this.serverless.cli.log(
             `λ without SQS event source: ${configDLQ[functionName].onError}.`,
-          );
-          return;
+          )
+          return
         }
 
         this.serverless.cli.log(
           `Creating DLQ of SQS event source, λ: ${functionName}.`,
-        );
+        )
 
-        let attempts = 0;
-        let QueueUrl;
+        let attempts = 0
+        let QueueUrl
         while (!QueueUrl && attempts < GET_QUEUES_MAX_RETRIES) {
-          attempts += 1;
+          attempts += 1
           try {
-            ({QueueUrl} = await client
+            ;({ QueueUrl } = await client
               .getQueueUrl({
                 QueueName: extractQueueNameFromARN(QueueArn),
               })
-              .promise());
+              .promise())
           } catch (e) {
             if (attempts === GET_QUEUES_MAX_RETRIES) {
-              throw e;
+              throw e
             }
           }
-          await sleep(1000);
+          await sleep(1000)
         }
         try {
           await client
@@ -103,17 +106,17 @@ class ServerlessOfflineSQSDLQ {
                 }),
               },
             })
-            .promise();
+            .promise()
         } catch (e) {
-          this.serverless.cli.log(e.message);
+          this.serverless.cli.log(e.message)
         }
-      });
+      })
     }
   }
 
   offlineStartEnd() {
-    this.serverless.cli.log('offline-start-end');
+    this.serverless.cli.log('offline-start-end')
   }
 }
 
-module.exports = ServerlessOfflineSQSDLQ;
+module.exports = ServerlessOfflineSQSDLQ
